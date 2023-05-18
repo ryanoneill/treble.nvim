311ba097e74564f67b0e69f044746caeda7ba9be
Ryan O'Neill <ryanoneill@gmail.com>
2023-05-17 23:16:49 -0700

    Initial Working Version

--- lua/treble/init.lua
+++ lua/treble/init.lua
@@ -1,89 +1,117 @@
 local M = {}
 
 local vim = vim
-local filter = vim.tbl_filter
-local map = vim.tbl_map
 
-local devicons = require("nvim-web-devicons")
+local pickers = require("telescope.pickers")
 local entry_display = require("telescope.pickers.entry_display")
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+local finders = require("telescope.finders")
+local config = require("telescope.config")
 
-function setup(opts)
+local function list_buffers()
+  local utils = require("bufferline.utils")
+  return utils.get_valid_buffers()
+end
+
+local function get_buffer_name(buffer_number)
+  return vim.api.nvim_buf_get_name(buffer_number)
+end
+
+local function buffer_file_name(buffer_name)
+  return vim.fn.fnamemodify(buffer_name, ":p:t")
+end
+
+local function buffer_icons(buffer_name)
+  local devicons = require("nvim-web-devicons")
+  return devicons.get_icon(buffer_name, string.match(buffer_name, "%a+$"), { default = true })
+end
+
+local function make_buffer_entries(buffer_numbers)
+
+  local results = {}
+  for buffer_index, buffer_number in ipairs(buffer_numbers) do
+    local buffer_name = get_buffer_name(buffer_number)
+    local file_name = buffer_file_name(buffer_name)
+    local devicons, devicons_highlight = buffer_icons(buffer_name)
+    local entry = {
+      buffer_index = buffer_index,
+      buffer_number = buffer_number,
+      buffer_name = buffer_name,
+      file_name = file_name,
+      devicons = devicons,
+      devicons_highlight = devicons_highlight
+    }
+    table.insert(results, entry)
+  end
+
+  return results
+end
+
+local function icon_width()
+  return 5
+end
+
+local function buffer_number_width()
+  return 5
+end
+
+local function buffer_name_width()
+  return 40
+end
+
+local function buffers(opts)
   opts = opts or {}
 
-  local default_icons, _ = devicons.get_icon("file", "", {default=true})
-
-  local bufnrs = filter(function(b)
-    return 1 == vim.fn.buflisted(b)
-  end, vim.api.nvim_list_bufs())
-
-  local max_bufnr = math.max(unpack(bufnrs))
-  local bufnr_width = #tostring(max_bufnr)
-
-  local max_bufname = math.max(
-    unpack(
-      map(function(bufnr)
-        return vim.fn.strdisplaywidth(vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":p:t"))
-      end, bufnrs)
-    )
-  )
-
+  local buffer_numbers = list_buffers()
+  local buffer_entries = make_buffer_entries(buffer_numbers)
   local displayer = entry_display.create {
     separator = " ",
     items = {
-      { width = bufnr_width },
-      { width = 4 },
-      { width = vim.fn.strwidth(default_icons) },
-      { width = max_bufname },
-      { remaining = true },
-    },
+      { width = buffer_number_width() },
+      { width = icon_width() },
+      { width = buffer_name_width() },
+    }
   }
 
   local make_display = function(entry)
     return displayer {
-      { entry.bufnr, "TelescopeResultsNumber" },
-      { entry.indicator, "TelescopeResultsComment" },
+      { entry.buffer_index, "TelescopeResultsNumber" },
       { entry.devicons, entry.devicons_highlight },
-      entry.file_name,
-      { entry.dir_name, "Comment" }
+      entry.file_name
     }
   end
 
-  return function(entry)
-    local bufname = entry.info.name ~= "" and entry.info.name or "[No Name]"
-    local hidden = entry.info.hidden == 1 and "h" or "a"
-    local readonly = vim.api.nvim_buf_get_option(entry.bufnr, "readonly") and "=" or " "
-    local changed = entry.info.changed == 1 and "+" or " "
-    local indicator = entry.flag .. hidden .. readonly .. changed
-    local dir_name = vim.fn.fnamemodify(bufname, ":p:h")
-    local file_name = vim.fn.fnamemodify(bufname, ":p:t")
-    local icons, highlight = devicons.get_icon(bufname, string.match(bufname, "%a+$"), { default = true })
-
-    return {
-      valid = true,
-
-      value = bufname,
-      ordinal = entry.bufnr .. " : " .. file_name,
-      display = make_display,
-      bufnr = entry.bufnr,
-
-      lnum = entry.info.lnum ~= 0 and entry.info.lnum or 1,
-      indicator = indicator,
-      devicons = icons,
-      devicons_highlight = highlight,
-
-      file_name = file_name,
-      dir_name = dir_name,
-    }
-  end
+  pickers.new(opts, {
+    prompt_title = "buffers",
+    attach_mappings = function(prompt_buffer_number, _)
+      actions.select_default:replace(function()
+        actions.close(prompt_buffer_number)
+        local selection = action_state.get_selected_entry()
+        vim.api.nvim_set_current_buf(selection.buffer_number)
+      end)
+      return true
+    end,
+    finder = finders.new_table {
+      results = buffer_entries,
+      entry_maker = function(entry)
+        return {
+          value = entry,
+          display = make_display,
+          ordinal = entry.buffer_index .. " : " .. entry.file_name,
+          buffer_index = entry.buffer_index,
+          buffer_number = entry.buffer_number,
+          buffer_name = entry.buffer_name,
+          file_name = entry.file_name,
+          devicons = entry.devicons,
+          devicons_highlight = entry.devicons_highlight
+        }
+      end
+    },
+    sorter = config.values.generic_sorter(opts),
+  }):find()
 end
 
-M.setup = setup
+M.buffers = buffers
 
 return M
-
--- -- Use in telescope buffers
-
--- require("telescope.builtin").buffers({
---   -- ...
---   entry_maker = require"vimrc.telescope.my_make_entry".gen_from_buffer_like_leaderf(),
--- })
